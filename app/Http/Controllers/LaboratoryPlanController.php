ef74a004824c93ad8518872c6ced5b285e673eaf
Carlos Jaldin <carjal192000@gmail.com>
2021-07-04 00:50:00 -0400

    laboratorios y planes migrations creado

---
+++ app/Http/Controllers/LaboratoryPlanController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LaboratoryPlan;
+use Illuminate\Http\Request;
+
+class LaboratoryPlanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\LaboratoryPlan  $laboratoryPlan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(LaboratoryPlan $laboratoryPlan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\LaboratoryPlan  $laboratoryPlan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(LaboratoryPlan $laboratoryPlan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\LaboratoryPlan  $laboratoryPlan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, LaboratoryPlan $laboratoryPlan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\LaboratoryPlan  $laboratoryPlan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(LaboratoryPlan $laboratoryPlan)
+    {
+        //
+    }
+}
---
+++ database/seeders/PlanSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PlanSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('plans')->insert([
+            [
+                'name' => 'Basico',
+                'price' => 100.0,
+                'description' => 'Este plan consiste en un plan basico por un mes'
+            ],
+            [
+                'name' => 'Ejecutivo',
+                'price' => 999.0,
+                'description' => 'Este plan consiste en un plan para empresas medianas/grandes, es el servicio completo por un anio entero'
+            ],
+            [
+                'name' => 'Vitalicio',
+                'price' => 1999.0,
+                'description' => 'Este plan consiste en un plan para laboratorios consolidados, apertura nuestro servicio para todo el recorrido de su empresa'
+            ],
+        ]);
+    }
+}
